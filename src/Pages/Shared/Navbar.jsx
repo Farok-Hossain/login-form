762fde32b0322e500a03e0f3272165d8854a7edf
Farok Hossain <farokafs7@gmail.com>
2024-10-24 22:19:46 +0600

    updated Navbar

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className="bg-blue-500 h-11  ">
       <div className="flex justify-center gap-10 font-bold text-white text-xl uppercase">
-        <Link to="/home">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/order">Order</Link>
         {user ? (
